eea9378557c0fe573df07db522443d469f306d30
UdonDa <udoooon0727@gmail.com>
2020-08-02 21:03:04 +0900

    update

--- deploy.sh
+++
@@ -1,1 +0,0 @@
-rsync -davzru --include-from <(git ls-files) --exclude .git --exclude-from <(git ls-files -o --directory) . im00:/home/yanai-lab/horita-d/im2im/rbsic/
--- udon.sh
+++
@@ -1,1 +0,0 @@
-rsync -davzru --include-from <(git ls-files) --exclude .git --exclude-from <(git ls-files -o --directory) . udon:/home/udon/programing/pfn/rbsic/
